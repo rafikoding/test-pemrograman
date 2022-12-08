dc3ec41c5c2d7f505f523e92aa82de1333202425
rafikoding <114796556+rafikoding@users.noreply.github.com>
2022-12-08 15:50:40 +0700

    Create segitiga.java

---
+++ TugasSesi8/segitiga.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas1;
+import javax.swing.*;
+
+/**
+ *
+ * @author Muhammad Rafi
+ */
+class segitiga{
+    double alas;
+    double tinggi;
+    
+}
+public class segitga {
+    public static void main(String args[]) {
+        
+        double volume;
+        segitiga s = new segitiga();
+        
+        s.alas = 20;
+        s.tinggi = 12;
+        
+        volume = s.alas * s.tinggi;
+        
+        System.out.println("Volume segitiga = " + volume);
+       
+    }
+    
+}
+    
+
